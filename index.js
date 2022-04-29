10a6029ff50b3d81588749d5ce0c050eca1eee59
herr-code <herrwebj@gmail.com>
2022-04-28 21:40:28 -0500

    Paso 3.2 Creacion del archivo principal index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+// importa tu clase, instancia al menos tres objetos e imprime los 
+//atributos name de todos.
+
+const MissionCommander = require('./app/missionCommander');
+
+// Objeto 1
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
+
+// Objeto 2
+const herrCode = new MissionCommander("Herr-code")
+console.log(herrCode.name)
+
+// Objeto 3
+const teresa = new MissionCommander("Teresa")
+console.log(teresa.name)
